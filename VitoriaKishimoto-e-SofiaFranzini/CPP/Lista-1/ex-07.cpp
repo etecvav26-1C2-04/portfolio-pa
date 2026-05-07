2e269d36e11e9d5192844e2cf346c2f03ba6f916
Sofia Franzini <sofiafranzini12@gmail.com>
2026-05-07 13:27:08 -0300

    Update ex-07.cpp

--- VitoriaKishimoto-e-SofiaFranzini/CPP/Lista-1/ex-07.cpp
+++ VitoriaKishimoto-e-SofiaFranzini/CPP/Lista-1/ex-07.cpp
@@ -1,20 +1,32 @@
+/* 
+Data: 07/05/2026
+Autores: Sofia Franzini e Vitória Kishimoto 
+Descrição: Ler a quantidade de pessoas e ler a altura dessas pessoas. Calcular e exibir a altura média de todas as pessoas. Somar todas as alturas;
+dividir pela quantidade de pessoas.
+*/
+
 #include <iostream>
+
 using namespace std;
 
 int main() {
-    int quantidade;
-    float altura, soma = 0, media;
-
+	
+    int quantidade, i;
+    float soma=0, altura, media;
+	
+	cout << "Digite a quantidade de pessoas: ";
     cin >> quantidade;
 
     for (int i = 1; i <= quantidade; i++) {
+    	
+    	cout << "Digite a altura da pessoa: ";
         cin >> altura;
-        soma += altura;
+        soma= soma + altura;
     }
 
     media = soma / quantidade;
 
-    cout << "Media das alturas: " << media << endl;
+    cout << "Media das alturas: " << media << "\n\n";
 
     return 0;
 }
