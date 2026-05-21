c86f961fe1b91621f70bcd6a6c8269e175ac4a7c
GabiOnaga <gabi_onaga@icloud.com>
2026-05-21 13:24:37 -0300

    Create ex-02.cpp

---
+++ GiovannaCostaCabral-e-GabrielaOnaga/CPP/Lista-2/parte1/ex-02.cpp
@@ -0,0 +1,35 @@
+/*
+   Data: 21/05/2026
+   Autores: Gabriela Onaga e Giovanna Costa 
+   Finalidade: Ler a base e o expoente; calcular a potência usando repetição;
+   Criar uma função para o cálculo.
+*/
+
+#include <iostream>
+using namespace std;
+
+int resultado (int base, int expoente) {
+    
+    int res=1;
+    
+    for (int i=1; i <= expoente;i++) {
+        
+    res = res * base;
+    
+    }
+    return res;
+}
+
+int main() {
+    int base, expoente;
+    
+    cout << "Digite a base: " << endl;
+    cin >> base;
+    
+    cout << "Digite o expoente: " << endl;
+    cin >> expoente;
+    
+    cout << "O valor da potencia eh " << resultado (base, expoente) << endl;
+
+    return 0;
+}
