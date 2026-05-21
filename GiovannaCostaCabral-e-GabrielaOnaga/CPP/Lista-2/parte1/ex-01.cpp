6b7bf063284c4227129cb0fddf72becfb217be7c
GabiOnaga <gabi_onaga@icloud.com>
2026-05-21 13:18:40 -0300

    Create ex-01.cpp

---
+++ GiovannaCostaCabral-e-GabrielaOnaga/CPP/Lista-2/parte1/ex-01.cpp
@@ -0,0 +1,30 @@
+/*
+   Data: 19/05/2026
+   Autores: Gabriela Onaga e Giovanna Costa 
+   Finalidade:  Ler um número inteiro.
+Exibir a soma dos números ímpares até o número lido.
+Criar uma função que retorne verdadeiro para números ímpares.
+*/
+
+#include <iostream>
+using namespace std;
+
+bool impar (int n) {
+    return n % 2 !=0;
+    }
+    
+int main() {
+    int numero, soma=0;
+    
+    cout << "Digite um numero inteiro: ";
+    cin >> numero;
+    
+    for (int i=1; i <= numero; i++) {
+        if (impar (i)) {
+            soma += i;
+        }
+    }
+   cout << "A soma dos numeros impares ate " << numero << endl <<"eh de: " << soma << endl;
+   
+   return 0;
+}
