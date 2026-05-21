742130d5199d6a7ad1bd19cbf13274a3d13478d2
Vitória Kishimoto <kishimoto.vitoria@gmail.com>
2026-05-21 10:42:31 -0300

    Add power calculation program in ex-02.cpp

---
+++ VitoriaKishimoto-e-SofiaFranzini/CPP/Lista-2/ex-02.cpp
@@ -0,0 +1,29 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int resultado (int base, int expoente) {
+    
+    int res=1;
+    
+    for (int i=1; i <= expoente;i++) {
+        
+    res = res * base;
+    
+    }
+    return res;
+}
+
+int main() {
+    int base, expoente;
+    
+    cout << "Digite a base: " << endl;
+    cin >> base;
+    
+    cout << "Digite o expoente: " << endl;
+    cin >> expoente;
+    
+    cout << "O valor da potencia eh " << resultado (base, expoente) << endl;
+
+    return 0;
+}
