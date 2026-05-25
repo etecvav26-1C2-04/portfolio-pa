b5e1874cf321f5ceed3b7e661d1eab47f527c65c
Vitória Kishimoto <kishimoto.vitoria@gmail.com>
2026-05-24 23:11:28 -0300

    Create ex-04.cpp

---
+++ VitoriaKishimoto-e-SofiaFranzini/CPP/Lista-2/parte1/ex-04.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int soma(int n1, int n2)
+{
+    return n1 + n2;
+}
+
+int sub(int n1, int n2)
+{
+    return n1 - n2;
+}
+
+int multip(int n1, int n2)
+{
+    return n1 * n2;
+}
+
+// Como a função usa int, divisões com decimal perdem a parte depois da vírgula
+int divisao(int n1, int n2)
+{
+    return n1 / n2;
+}
+
+int main()
+{
+    int num1, num2;
+
+    cout << "Digite o primeiro numero: ";
+    cin >> num1;
+
+    cout << "Digite o segundo numero: ";
+    cin >> num2;
+
+    cout << "Resultado da soma: "           << soma(num1, num2)   << endl;
+    cout << "Resultado da subtracao: "      << sub(num1, num2)    << endl;
+    cout << "Resultado da multiplicacao: "  << multip(num1, num2) << endl;
+    cout << "Resultado da divisao: "        << divisao(num1, num2)<< endl;
+
+    return 0;
+}
