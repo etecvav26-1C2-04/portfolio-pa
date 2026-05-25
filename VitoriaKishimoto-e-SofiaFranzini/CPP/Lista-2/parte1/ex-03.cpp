9160ac4462ad4b5edda806ca2d8560806cdaf1df
Vitória Kishimoto <kishimoto.vitoria@gmail.com>
2026-05-24 22:42:37 -0300

    Add ideal weight calculator based on gender and height
    This program calculates the ideal weight based on gender and height. It includes functions for both male and female calculations.

---
+++ VitoriaKishimoto-e-SofiaFranzini/CPP/Lista-2/parte1/ex-03.cpp
@@ -0,0 +1,43 @@
+/* Data: 24/05/2026
+Autores: Vitória Kishimoto e Sofia Franzini
+Finalidade:Ler o sexo e a altura.
+Calcular o peso ideal. Criar uma função para cada caso.
+*/
+
+#include <iostream>
+using namespace std;
+
+float pesoMasculino(float altura)
+{
+    return 72.7 * altura - 58;
+}
+
+float pesoFeminino(float altura)
+{
+    return 62.1 * altura - 44.7;
+}
+
+int main()
+{
+    float altura, peso;
+    string sexo;
+
+    cout << "Digite seu sexo: ";
+    cin >> sexo;
+
+    cout << "Digite sua altura: ";
+    cin >> altura;
+
+    if (sexo == "Masculino")
+    {
+        peso = pesoMasculino(altura);
+    }
+    else
+    {
+        peso = pesoFeminino(altura);
+    }
+
+    cout << "Peso ideal: " << peso << endl;
+
+    return 0;
+}
