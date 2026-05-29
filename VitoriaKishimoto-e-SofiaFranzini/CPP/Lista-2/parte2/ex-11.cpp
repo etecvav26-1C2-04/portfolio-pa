2455a4d248cdb5df4d7c851139036734709171b2
Vitória Kishimoto <kishimoto.vitoria@gmail.com>
2026-05-28 22:55:36 -0300

    Update ex-11.cpp

--- VitoriaKishimoto-e-SofiaFranzini/CPP/Lista-2/parte2/ex-11.cpp
+++ VitoriaKishimoto-e-SofiaFranzini/CPP/Lista-2/parte2/ex-11.cpp
@@ -12,19 +12,17 @@ using namespace std;
 int main() {
     int v[10];
 
-    // 1) Ler os 10 números
+    // aq leu os 10 números
     for (int i = 0; i < 10; i++) {
         cout << "Digite v[" << i << "]: ";
         cin  >> v[i];
     }
-
-    // 2) Substituir negativos por zero
+    
     for (int i = 0; i < 10; i++) {
         if (v[i] < 0)
             v[i] = 0;
     }
 
-    // 3) Exibir o vetor
     cout << "\nVetor: ";
     for (int i = 0; i < 10; i++) {
         cout << v[i] << "  ";
