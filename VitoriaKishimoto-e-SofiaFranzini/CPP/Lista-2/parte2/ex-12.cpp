fbdaf05a72397db142a44b49b21a92adeae5e61b
Vitória Kishimoto <kishimoto.vitoria@gmail.com>
2026-05-28 22:54:07 -0300

    Update ex-12.cpp

--- VitoriaKishimoto-e-SofiaFranzini/CPP/Lista-2/parte2/ex-12.cpp
+++ VitoriaKishimoto-e-SofiaFranzini/CPP/Lista-2/parte2/ex-12.cpp
@@ -16,9 +16,10 @@ bool impar (int n) {
 
 int main() {
     int v[15];
+    int qtdPares = 0, qtdImpares = 0;
 
     // aq leu os 15 números
-    for (int i = 1; i < 15; i++) {
+    for (int i = 0; i < 15; i++) {
         cout << "Digite v[" << i +1<< "]: ";
         cin  >> v[i];
     }
