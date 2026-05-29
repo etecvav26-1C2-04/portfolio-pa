d767fe89331428de26e587d055da68946290115f
GabiOnaga <gabi_onaga@icloud.com>
2026-05-29 20:37:14 -0300

    Add program to read and process an array of numbers
    This program reads 10 numbers into an array, replaces negative numbers with zero, and displays the array.

---
+++ GiovannaCostaCabral-e-GabrielaOnaga/CPP/Lista-2/parte2/ex-11.cpp
@@ -0,0 +1,33 @@
+/*
+Data: 29/05/2026
+Autores: Gabriela Onaga e Giovanna Costa 
+Finalidade: Ler 10 números em um vetor.
+Substituir números negativos por zero.
+Exibir o vetor.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int v[10];
+
+    // aq leu os 10 números
+    for (int i = 0; i < 10; i++) {
+        cout << "Digite v[" << i << "]: ";
+        cin  >> v[i];
+    }
+    
+    for (int i = 0; i < 10; i++) {
+        if (v[i] < 0)
+            v[i] = 0;
+    }
+
+    cout << "\nVetor: ";
+    for (int i = 0; i < 10; i++) {
+        cout << v[i] << "  ";
+    }
+    cout << endl;
+
+    return 0;
+}
