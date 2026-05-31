25abb42cd5e43df7cc8e15cf74e48b1562f45ac0
GabiOnaga <gabi_onaga@icloud.com>
2026-05-31 16:43:15 -0300

    Create ex-13.cpp

---
+++ GiovannaCostaCabral-e-GabrielaOnaga/CPP/Lista-2/parte2/ex-13.cpp
@@ -0,0 +1,41 @@
+/* Data: 31/05/2026
+Autores: Gabriela Onaga e Giovanna Costa
+Finalidade: Ler 20 letras em um vetor.
+Ler uma letra e contar quantas vezes ela aparece.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char letras[20];
+    char busca;
+    int contador = 0;
+
+    // Ler as 20 letras
+    for(int i = 0; i < 20; i++)
+    {
+        cout << "Digite uma letra: ";
+        cin >> letras[i];
+    }
+
+    // Ler a letra que será procurada
+    cout << "Digite a letra que deseja contar: ";
+    cin >> busca;
+
+    // Contar quantas vezes a letra aparece
+    for(int i = 0; i < 20; i++)
+    {
+        if(letras[i] == busca)
+        {
+            contador++;
+        }
+    }
+
+    // Mostrar resultado
+    cout << "A letra " << busca << " apareceu "
+         << contador << " vezes.";
+
+    return 0;
+}
