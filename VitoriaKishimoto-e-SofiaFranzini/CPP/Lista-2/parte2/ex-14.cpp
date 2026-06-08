0b61782b1ce7c8cc67bbfd9f9c3e8054d0da900a
Vitória Kishimoto <kishimoto.vitoria@gmail.com>
2026-06-08 15:11:34 -0300

    Create ex-14.cpp

---
+++ VitoriaKishimoto-e-SofiaFranzini/CPP/Lista-2/parte2/ex-14.cpp
@@ -0,0 +1,46 @@
+/*
+Data: 08/06/2026
+Autores: Vitória Kishimoto e Sofia Franzini
+Finalidade:  Ler 10 nomes em um vetor.
+Ordenar em ordem decrescente e exibir em ordem crescente
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string nomes[10];
+    string aux;
+    
+    
+    for(int i = 0; i < 10; i++)
+    {
+        cout << "Digite um nome: ";
+        cin >> nomes[i];
+    }
+
+    for(int i = 0; i < 10; i++)
+    {
+        for(int j = i + 1; j < 10; j++)
+        {
+            if(nomes[i] > nomes[j])
+            {
+                aux = nomes[i];
+                nomes[i] = nomes[j];
+                nomes[j] = aux;
+            }
+        }
+    }
+
+    cout << "\nNomes em ordem crescente:\n";
+
+    for(int i = 0; i < 10; i++)
+    {
+        cout << nomes[i] << endl;
+    }
+
+    return 0;
+}
