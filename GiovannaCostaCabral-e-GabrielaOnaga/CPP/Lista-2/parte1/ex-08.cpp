ee060b00329301faad0bdd900f1bf7ba00ba0686
giovanna <giovannaccabral2@gmail.com>
2026-06-17 20:43:42 -0300

    Atualizar o ex-08.cpp

--- GiovannaCostaCabral-e-GabrielaOnaga/CPP/Lista-2/parte1/ex-08.cpp
+++ GiovannaCostaCabral-e-GabrielaOnaga/CPP/Lista-2/parte1/ex-08.cpp
@@ -1,1 +1,30 @@
-.
+/* 
+Data: 06/06/2026
+Autores: Gabriela Onaga e Giovanna Costa
+Descrição:
+*/
+
+#include <iostream>
+using namespace std;
+
+// Função que calcula a área do círculo
+double areaCirculo(double raio) {
+    return 3.14 * raio * raio; // fórmula: πr²
+}
+
+int main() {
+    double raio, soma = 0;
+
+    cout << "Digite o raio de 5 circulos:\n";
+
+    for(int i = 0; i < 5; i++) {
+        cin >> raio;
+
+        // Somando as áreas usando a função
+        soma += areaCirculo(raio);
+    }
+
+    cout << "Soma das areas: " << soma << endl;
+
+    return 0;
+}
