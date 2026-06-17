953d6fb2db2a6878ebf85afef77d5e678cea427b
giovanna <giovannaccabral2@gmail.com>
2026-06-17 20:23:16 -0300

    Atualizar o ex-06.cpp

--- GiovannaCostaCabral-e-GabrielaOnaga/CPP/Lista-2/parte1/ex-06.cpp
+++ GiovannaCostaCabral-e-GabrielaOnaga/CPP/Lista-2/parte1/ex-06.cpp
@@ -1,1 +1,34 @@
-.
+/* 
+Data: 05/05/2026
+Autores: Giovanna Cabral e Gabriela Onaga 
+Descrição: Ler vários números até que a soma desses números seja maior que 100. Exibir a multiplicação dos números lidos e
+parar quando a soma passar de 100; multiplicar todos os números lidos até esse ponto
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+        int numero;
+        int soma=0;
+        long produto=1;
+
+        while (soma <=100){
+
+        cout << "Digite o número: ";
+        cin >> numero;
+
+
+        soma += numero;
+        produto*= numero;
+
+        }
+
+
+        cout << "Produto final: " << produto << "\n\n";
+
+
+        return 0;
+}
