1b7eda24c12d9a7009d79e2c8c60acc437f02628
giovanna <giovannaccabral2@gmail.com>
2026-06-17 20:44:55 -0300

    Atualizar o ex-09.cpp

--- GiovannaCostaCabral-e-GabrielaOnaga/CPP/Lista-2/parte1/ex-09.cpp
+++ GiovannaCostaCabral-e-GabrielaOnaga/CPP/Lista-2/parte1/ex-09.cpp
@@ -1,1 +1,34 @@
-.
+/* 
+Data: 06/06/2026
+Autores: Gabriela Onaga e Giovanna Costa 
+Descrição: 
+*/
+
+
+#include <iostream>
+using namespace std;
+
+// Função que substitui vogais por '*'
+string substituirVogais(string palavra) {
+    for(int i = 0; i < palavra.length(); i++) {
+        char c = tolower(palavra[i]);
+
+        // Verificando se é vogal
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            palavra[i] = '*';
+        }
+    }
+    return palavra;
+}
+
+int main() {
+    string palavra;
+
+    cout << "Digite uma palavra: ";
+    cin >> palavra;
+
+    // Chamando a função
+    cout << "Resultado: " << substituirVogais(palavra) << endl;
+
+    return 0;
+}
