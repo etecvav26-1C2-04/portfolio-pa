9574ec65725325d856381fb70a86857bd7faa6d9
giovanna <giovannaccabral2@gmail.com>
2026-06-18 13:23:51 -0300

    Atualizar o ex-15.cpp

--- GiovannaCostaCabral-e-GabrielaOnaga/CPP/Lista-2/parte2/ex-15.cpp
+++ GiovannaCostaCabral-e-GabrielaOnaga/CPP/Lista-2/parte2/ex-15.cpp
@@ -1,1 +1,38 @@
+/*
+   Autores: Gabriela Onaga e Giovanna Costa
+   Data: 21/05/2026
+   Finalidade:  Ler 20 números em um vetor.
+                Exibir o índice do primeiro número negativo.
+*/
 
+
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+    //Criacao do vetor(array)
+    int arr[20];
+
+
+    //Laço de repetição para ler os 20 numeros
+    for(int i = 0; i < 20; i++){
+        cout << "Digite o numero " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    //Laço de repetição para achar o primeiro numero negativo
+    for(int i = 0; i < 20; i++){
+        int numero = arr[i];
+        if(numero < 0){
+            cout << "O indice do primeiro numero negativo eh: " << i;
+        }
+    }
+
+
+
+
+
+    return 0;
+}
