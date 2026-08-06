336d03f21784fc71c5d8be1b49ce1db94c6a3500
etecvav26-1C2-04 <codegirls.etec@gmail.com>
2026-08-06 15:13:18 -0300

    alteração

---
+++ Vitoria-e-Sofia/Python/python-conversion/cpp/ex01.cpp
@@ -0,0 +1,33 @@
+/*
+    Autores: Vitória Kishimoto e Sofia Franzini
+    Data: 06/08/2026
+    Descrição: Lê um número inteiro e informa se ele é positivo, negativo ou zero.
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numero;
+
+    cout << "Digite um numero inteiro: ";
+    cin >> numero;
+
+    if (numero > 0)
+    {
+        cout << "O numero e positivo." << endl;
+    }
+    else if (numero < 0)
+    {
+        cout << "O numero e negativo." << endl;
+    }
+    else
+    {
+        cout << "O numero e zero." << endl;
+    }
+
+    return 0;
+}
+
