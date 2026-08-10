aa6f02b400599e869b723a402d71d6f92bf03b66
Vitória Kishimoto <kishimoto.vitoria@gmail.com>
2026-08-10 09:44:20 -0300

    Update ex01.py

--- Vitoria-e-Sofia/Python/python-conversion/python/ex01.py
+++ Vitoria-e-Sofia/Python/python-conversion/python/ex01.py
@@ -4,5 +4,14 @@
     Descrição: Lê um número inteiro e informa se ele é positivo, negativo ou zero.
 */
 
+numero= int(input("Digite um número inteiro: "))
+
+if numero > 0:
+    print ("O número é positivo")
+elif numero < 0:
+    print ("O número é negativo")
+else:
+    print ("O número é zero")
+
 
 
