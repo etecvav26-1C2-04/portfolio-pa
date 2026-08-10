42c9ca063221fbf1cc2bac03734c99e5bf08fa28
vikishimoto <kishimoto.vitoria@gmail.com>
2026-08-10 10:39:46 -0300

    ex py

--- Vitoria-e-Sofia/Python/python-conversion/python/ex02.py
+++ Vitoria-e-Sofia/Python/python-conversion/python/ex02.py
@@ -3,3 +3,13 @@
     Data: 10/08/26
     Descrição: Lê dois números inteiros e informa qual é o maior ou se são iguais.
 """
+
+numero1= int(input("Digite o primeiro número: "))
+numero2= int(input("Digite o segundo número: "))
+
+if numero1 > numero2 :
+    print (("O maior número é: ") numero1)
+elif numero2 > numero1 :
+    print (("O maior número é: ") numero2)  
+else :
+    print ("Os números são iguais") 
--- Vitoria-e-Sofia/Python/python-conversion/python/ex03.py
+++ Vitoria-e-Sofia/Python/python-conversion/python/ex03.py
@@ -3,3 +3,4 @@
     Data: 10/08/26
     Descrição: Lê um número inteiro positivo e exibe a contagem de 1 até esse número.
 """
+
