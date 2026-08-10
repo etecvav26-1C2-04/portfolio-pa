08172d92200db2a425862156201bd38297f52cef
vikishimoto <kishimoto.vitoria@gmail.com>
2026-08-10 10:49:18 -0300

    ex03 py

--- Vitoria-e-Sofia/Python/python-conversion/python/ex03.py
+++ Vitoria-e-Sofia/Python/python-conversion/python/ex03.py
@@ -4,3 +4,14 @@
     Descrição: Lê um número inteiro positivo e exibe a contagem de 1 até esse número.
 """
 
+numero= int(input("Digite um número inteiro positivo: "))
+contador= 1
+
+if numero > 0 :
+    while contador <= numero:
+        print (contador)
+        contador += 1
+
+else : 
+    print ("Número inválido. Digite um valor positivo")
+    
