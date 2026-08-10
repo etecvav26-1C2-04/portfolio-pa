f62eb6d5798ab2d7974c6a60947841d5022bc503
Vitória Kishimoto <kishimoto.vitoria@gmail.com>
2026-08-10 09:46:08 -0300

    Clean up comments in ex01.py
    Removed commented block at the beginning of the file.

--- Vitoria-e-Sofia/Python/python-conversion/python/ex01.py
+++ Vitoria-e-Sofia/Python/python-conversion/python/ex01.py
@@ -1,8 +1,8 @@
-/*
+"""
     Autores: Vitória Kishimoto e Sofia Franzini
     Data: 06/08/2026
     Descrição: Lê um número inteiro e informa se ele é positivo, negativo ou zero.
-*/
+"""
 
 numero= int(input("Digite um número inteiro: "))
 
