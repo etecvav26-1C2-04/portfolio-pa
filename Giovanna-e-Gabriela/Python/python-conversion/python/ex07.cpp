4e432c253c8714889ae38b8fa9f7aeaa3691b02d
GabiOnaga <gabi_onaga@icloud.com>
2026-08-11 10:07:56 -0300

    Create ex07.cpp

---
+++ Giovanna-e-Gabriela/Python/python-conversion/python/ex07.cpp
@@ -0,0 +1,26 @@
+"""
+Autor: Gabriela Onaga e Giovanna Costa 
+Data: 11/08/2026
+Descrição: Descrição: Lê 10 números e conta quantos são positivos, negativos e zeros.
+"""
+
+contador = 1
+positivo = 0
+negativo = 0
+zeros = 0
+
+while contador <= 10:
+    numero = int(input(f"Digite o {contador}º número: "))
+
+    if numero > 0:
+        positivo += 1
+    elif numero < 0:
+        negativo += 1
+    else:
+        zeros += 1
+
+    contador += 1
+
+print(f"Positivos: {positivo}")
+print(f"Negativos: {negativo}")
+print(f"Zeros: {zeros}")
