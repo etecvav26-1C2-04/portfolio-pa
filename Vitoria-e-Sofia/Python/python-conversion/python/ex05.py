681f2c72d900423280340531ae5340165c8c80dd
Vitória Kishimoto <kishimoto.vitoria@gmail.com>
2026-08-10 22:03:02 -0300

    Update ex05.py

--- Vitoria-e-Sofia/Python/python-conversion/python/ex05.py
+++ Vitoria-e-Sofia/Python/python-conversion/python/ex05.py
@@ -3,3 +3,21 @@
     Data: 10/08/26
     Descrição: Lê um número de 1 a 10 e exibe sua tabuada, validando a entrada.
 """
+
+numero= int(input("Digite um numero de 1 a 10:"))
+contador = 1
+
+while numero < 1 or numero >10:
+    numero = int(input("Valor invalido. Digite novamente: "))
+    
+while contador <= 10:
+    print(numero, "x", contador, "=", numero * contador)
+    contador += 1
+
+
+
+
+
+
+    
+    
