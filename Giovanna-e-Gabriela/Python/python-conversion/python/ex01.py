3c591e0fd0007ff2806fabe6399dda8e02ca8f6c
Giovanna Cabral <giovannaccabral2@gmail.com>
2026-08-12 22:32:12 -0300

    Update ex01.py

--- Giovanna-e-Gabriela/Python/python-conversion/python/ex01.py
+++ Giovanna-e-Gabriela/Python/python-conversion/python/ex01.py
@@ -1,3 +1,9 @@
+""
+    Autores: Gabriela Onaga e Giovanna Costa
+    Data: 06/08/2026
+    Descrição: Lê um número inteiro e informa se ele é positivo, negativo ou zero.
+""
+
 num = int(input("Digite um número inteiro: "))
 
 if num > 0:
