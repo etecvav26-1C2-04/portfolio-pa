9f45d4cb79251c36625df76aee4be18cc26d4e2c
Giovanna Cabral <giovannaccabral2@gmail.com>
2026-08-12 23:27:43 -0300

    Update ex04.py

--- Giovanna-e-Gabriela/Python/python-conversion/python/ex04.py
+++ Giovanna-e-Gabriela/Python/python-conversion/python/ex04.py
@@ -1,1 +1,19 @@
+"""
+    Autor: Giovanna Cabral e Gabriela Onaga
+    Data: 12/08/2026
+    Descrição: Calcula a soma dos números pares de 1 até um número informado.
+"""
 
+num = int(input("Digite um numero inteiro positivo: "))
+contador = 1
+soma = 0
+
+if num > 0:
+    while contador <= num:
+        if contador % 2 == 0:
+            soma = soma + contador
+        contador += 1
+
+    print(f"Soma dos pares: {soma}")
+else:
+    print("Número Inválido.")
