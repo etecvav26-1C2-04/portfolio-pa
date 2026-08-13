3958ed50568e140a3e0672a9370976436a4f380c
Giovanna Cabral <giovannaccabral2@gmail.com>
2026-08-12 23:45:45 -0300

    Update ex01.py

--- Giovanna-e-Gabriela/Python/python-conversion/python/ex01.py
+++ Giovanna-e-Gabriela/Python/python-conversion/python/ex01.py
@@ -1,5 +1,5 @@
 """
-    Autores: Gabriela Onaga e Giovanna Costa
+    Autores: Giovanna Cabral e Gabriela Onaga
     Data: 06/08/2026
     Descrição: Lê um número inteiro e informa se ele é positivo, negativo ou zero.
 """
