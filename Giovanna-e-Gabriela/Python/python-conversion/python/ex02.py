ba24dab8b9441ba6fd264e43a89cf02a8f52a4a2
Giovanna Cabral <giovannaccabral2@gmail.com>
2026-08-12 23:44:46 -0300

    Update ex02.py

--- Giovanna-e-Gabriela/Python/python-conversion/python/ex02.py
+++ Giovanna-e-Gabriela/Python/python-conversion/python/ex02.py
@@ -1,18 +1,18 @@
 """
-    Autor: Giovanna Costa Cabral e Gabriela Onaga
+    Autor: Giovanna Cabral e Gabriela Onaga
     Data: 10/08/2026
     Descrição: Lê dois números inteiros e informa qual é o maior ou se são iguais.
 """
 
-numero1 = int(input("Digite o primeiro numero: "))
-numero2 = int(input("Digite o segundo numero: "))
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo numero: "))
 
 
-if numero1 > numero2:
-     print(f"O maior numero e: {numero1}")
+if num1 > num2:
+     print(f"O maior numero e: {num1}")
      
-elif numero2 > numero1:
-    print(f"O maior numero e: {numero2}")
+elif num2 > num1:
+    print(f"O maior numero e: {num2}")
 
 else:
     print(f"Os numeros sao iguais.")
