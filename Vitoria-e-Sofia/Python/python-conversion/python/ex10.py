8f3bcff3a355050a0ac60eb9e1c31252864c0d3b
Sofia Franzini <sofiafranzini12@gmail.com>
2026-08-13 12:32:00 -0300

    Create ex10.py

---
+++ Vitoria-e-Sofia/Python/python-conversion/python/ex10.py
@@ -0,0 +1,21 @@
+"""
+    Autor: Vitória Kishimoto e Sofia Franzini
+    Data: 12/08/2026
+    Descrição: Simula um sistema simples de senha com até 3 tentativas.
+"""
+
+senhaCorreta = 1234
+tentativas = 1
+limiteTentativas = 3
+
+senha = int(input("Digite a senha: "))
+
+while senha != senhaCorreta and tentativas < limiteTentativas:
+    senha = int(input("Senha incorreta. Tente novamente: "))
+    tentativas += 1
+
+if senha == senhaCorreta:
+    print("Acesso liberado.")
+else:
+    print("Acesso bloqueado.")
+    
