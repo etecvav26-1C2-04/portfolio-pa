82c229ac6977783c45b6ac014f11531cca7f4974
Giovanna Cabral <giovannaccabral2@gmail.com>
2026-08-12 23:08:46 -0300

    Update ex03.py

--- Giovanna-e-Gabriela/Python/python-conversion/python/ex03.py
+++ Giovanna-e-Gabriela/Python/python-conversion/python/ex03.py
@@ -1,1 +1,16 @@
+"""
+    Autor: Giovanna Cabral e Gabriela Onaga
+    Data: 11/08/2026
+    Descrição: Lê um número inteiro positivo e exibe a contagem de 1 até esse número.
+"""
 
+num = int(input("Digite um número inteiro positivo: "))
+contador = 1
+
+if num > 0:
+    while contador <= num:
+        print(contador)
+        contador += 1
+
+else: 
+    print("Numero invalido. Digite um valor positivo.") 
